75454726365289b6fa2be7525ff995628d772f88
Eleriin <eleriin.r@gmail.com>
2018-04-04 16:40:04 +0300

    threading

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,35 +1,23 @@
 package webserver;
-
-import java.io.File;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class WebServer {
-    public static void main(String[] args) throws Exception {
-        final int portNumber = 1337;
-        if (args.length == 0) {
-            throw new RuntimeException("Missing command line argument for server file directory");
-        }
-        System.out.println("Server file directory set as " + args[0]);
-        try (ServerSocket ss = new ServerSocket(portNumber)) {
-            System.out.println("Socket successfully initialized");
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket ss = new ServerSocket(1337)) {
+            if (args.length == 0) {
+                throw new RuntimeException("Missing command line argument for server file directory");
+            }
+            String fileName = args[0];
+            System.out.println("Server file directory set as " + fileName);
+            System.out.println("Ready for clients to connect");
             while (true) {
-                System.out.println("Awaiting new connection");
-                File directory = new File(args[0]);
-                if (!directory.isDirectory()) {
-                    throw new RuntimeException("Server file directory not found");
-                }
-                Socket socket = ss.accept();
-                Request request = new Request();
-                request.readRequest(socket);
-                System.out.println(request.getRequestLine());
-                Response response = new Response(directory, request);
-                String[] requestInfo = response.checkResponse();
-                if (requestInfo[0].equals("GET")) {
-                    response.getResponse(socket, requestInfo[1]);
-                } else if (requestInfo[0].equals("POST")) {
-                    response.postResponse(socket, requestInfo[1]);
-                }
+                Socket s = ss.accept();
+                Threads threads = new Threads(fileName, s);
+                Thread thread = new Thread(threads);
+                thread.start();
             }
         }
     }
