fa3fd2ab1fec01ef328543b13f8643f833ae3e43
taavi224 <taavi224@gmail.com>
2018-04-08 16:53:45 +0300

    Made file tree work for nested folders. TODO: Add support for names with spaces.

--- src/main/java/webserver/DirectoryBrowserGenerator.java
+++ src/main/java/webserver/DirectoryBrowserGenerator.java
@@ -6,6 +6,18 @@ import java.io.IOException;
 public class DirectoryBrowserGenerator {
 
     public static byte[] generate(File givenDir, File defaultDir) throws IOException {
+    	String relativeDirname = "";
+    	String parentDirname = "";
+    	String[] givenDirPathSplit = givenDir.getAbsolutePath().split("\\\\");
+    	String[] defaultDirPathSplit = defaultDir.getAbsolutePath().split("\\\\");
+
+	    for (int i = defaultDirPathSplit.length; i < givenDirPathSplit.length; i++) {
+		    relativeDirname = relativeDirname + "/" + givenDirPathSplit[i];
+		    if (i == givenDirPathSplit.length-2) {
+		    	parentDirname = relativeDirname;
+		    }
+	    }
+
     	String htmlContent = "<!DOCTYPE html>\n" +
 			    "<html lang=\"en\">\n" +
 			    "<head>\n" +
@@ -16,21 +28,13 @@ public class DirectoryBrowserGenerator {
 
 	    File[] filesInGivenDir = givenDir.listFiles();
 	    if (!givenDir.equals(defaultDir)) {
-	    	File parentFile = givenDir.getParentFile();
-	    	if (parentFile.equals(defaultDir)) {
-			    htmlContent = htmlContent + "<a href=\"http://localhost:1337/" + "\">" + "..." + "</a> \n <br>";
-		    } else {
-			    htmlContent = htmlContent + "<a href=\"http://localhost:1337/" + givenDir.getParentFile().getName() + "\">" + "..." + "</a> \n <br>";
-		    }
+		    htmlContent = htmlContent + "<a href=\"http://localhost:1337" + parentDirname + "\">" + "..." + "</a> \n <br>";
 	    }
 	    if (filesInGivenDir.length > 0) {
 		    for (File file : filesInGivenDir) {
-			    htmlContent = htmlContent + "<a href=\"http://localhost:1337/";
-			    if (givenDir.equals(defaultDir)) {
-				    htmlContent = htmlContent + file.getName() + "\"" +">" + file.getName() + "</a>\n <br>";
-			    } else {
-				    htmlContent = htmlContent + givenDir.getName() + "/" + file.getName() + "\">" + file.getName() + "</a>\n <br>";
-			    }
+			    htmlContent = htmlContent + "<a href=\"http://localhost:1337";
+			    htmlContent = htmlContent + relativeDirname + "/" + file.getName() + "\">" + file.getName() + "</a>\n <br>";
+
 		    }
 	    } else {
 	    	htmlContent = htmlContent + "<p>Empty directory</p>\n";
