3a597c7b84a6e0d7479e44b1dba7355a182026f7
Markus <markus.punnar@gmail.com>
2018-04-18 15:27:34 +0300

    Class for finding files from classpath

---
+++ src/main/java/webserver/ClasspathUtil.java
@@ -0,0 +1,20 @@
+package webserver;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ClasspathUtil {
+    public static byte[] readFileFromClasspath(String fileName) throws IOException {
+        try (InputStream is = WebServer.class.getClassLoader().getResourceAsStream(fileName)) {
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            int nRead;
+            byte[] data = new byte[1024];
+            while ((nRead = is.read(data, 0, data.length)) != -1) {
+                buffer.write(data, 0, nRead);
+            }
+            buffer.flush();
+            return buffer.toByteArray();
+        }
+    }
+}
