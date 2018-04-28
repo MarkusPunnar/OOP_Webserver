b50b42b37c87d61b886f904934dda0011d270a04
taavi224 <taavi224@gmail.com>
2018-04-28 16:27:59 +0300

    Reformatted main server code.

--- server/src/main/java/webserver/WebServer.java
+++ server/src/main/java/webserver/WebServer.java
@@ -10,37 +10,37 @@ import java.util.ServiceLoader;
 
 public class WebServer {
 
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket ss = new ServerSocket(1337)) {
-            String dirName;
-            Map<String, String> mimeTypes = new HashMap<>();
-            if (args.length == 0) {
-                throw new RuntimeException("Server file directory not assigned.");
-            } else {
-                dirName = args[0];
-            }
-            if (!new File(dirName).isDirectory()) {
-                throw new RuntimeException("Command line argument " + dirName + " is not a directory.");
-            }
-            System.out.println("Server file directory set as " + dirName);
-            System.out.println("Ready for clients to connect");
-            byte[] mimeTypesAsArray = WebServerUtil.readFileFromClasspath("extensions.txt");
-            String[] mimeTypesAsString = new String(mimeTypesAsArray, "UTF-8").split("\n");
-            for (String mimeType : mimeTypesAsString) {
-                String[] mimeInfo = mimeType.split(" ");
-                mimeTypes.put(mimeInfo[0], mimeInfo[1]);
-            }
-            Map<String, RequestHandler> dynamicResponseURIs = new HashMap<>();
-	        ServerConfig motherOfAllPlugins = new ServerConfig(Paths.get(dirName), mimeTypes, dynamicResponseURIs);
-            for (RequestHandler requestHandler : ServiceLoader.load(RequestHandler.class)) {
-                requestHandler.initialize(motherOfAllPlugins);
-                requestHandler.register(dynamicResponseURIs);
-            }
-            while (true) {
-                Socket socket = ss.accept();
-                Thread thread = new Thread(new HandleRequestAndSendResponse(socket, motherOfAllPlugins));
-                thread.start();
-            }
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		try (ServerSocket ss = new ServerSocket(1337)) {
+			String dirName;
+			Map<String, String> mimeTypes = new HashMap<>();
+			if (args.length == 0) {
+				throw new RuntimeException("Server file directory not assigned.");
+			} else {
+				dirName = args[0];
+			}
+			if (!new File(dirName).isDirectory()) {
+				throw new RuntimeException("Command line argument " + dirName + " is not a directory.");
+			}
+			System.out.println("Server file directory set as " + dirName);
+			System.out.println("Ready for clients to connect");
+			byte[] mimeTypesAsArray = WebServerUtil.readFileFromClasspath("extensions.txt");
+			String[] mimeTypesAsString = new String(mimeTypesAsArray, "UTF-8").split("\n");
+			for (String mimeType : mimeTypesAsString) {
+				String[] mimeInfo = mimeType.split(" ");
+				mimeTypes.put(mimeInfo[0], mimeInfo[1]);
+			}
+			Map<String, RequestHandler> dynamicResponseURIs = new HashMap<>();
+			ServerConfig motherOfAllPlugins = new ServerConfig(Paths.get(dirName), mimeTypes, dynamicResponseURIs);
+			for (RequestHandler requestHandler : ServiceLoader.load(RequestHandler.class)) {
+				requestHandler.initialize(motherOfAllPlugins);
+				requestHandler.register(dynamicResponseURIs);
+			}
+			while (true) {
+				Socket socket = ss.accept();
+				Thread thread = new Thread(new HandleRequestAndSendResponse(socket, motherOfAllPlugins));
+				thread.start();
+			}
+		}
+	}
 }
