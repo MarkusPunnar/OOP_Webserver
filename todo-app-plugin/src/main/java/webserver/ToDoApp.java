955114957e4056d5a87451dfa2a89ad241dc4764
Eleriin <eleriin.r@gmail.com>
2018-05-19 19:16:29 +0300

    streams closed

--- todo-app-plugin/src/main/java/webserver/ToDoApp.java
+++ todo-app-plugin/src/main/java/webserver/ToDoApp.java
@@ -10,11 +10,7 @@ public class ToDoApp implements RequestHandler{
     @Mapping(URI = "/todoapp/form", method = "POST")
     public Response handle(Request request) throws UnsupportedEncodingException {
         Map<String, String> responseHeaders = new HashMap<>();
-        Map<String, String> dataMap = request.bodyToForm();
-        if (dataMap == null) {
-            return new Response(StatusCode.BAD_REQUEST, Collections.emptyMap(), null);
-        }
-        String task = dataMap.get("user_message");
+        String task = request.bodyToForm().get("user_message");
         if(task!=null)
             toDoList.add(task);
         responseHeaders.put("Location", "/todoapp/form");
@@ -23,14 +19,15 @@ public class ToDoApp implements RequestHandler{
 
     @Mapping(URI = "/todoapp/form")
     public Response getList(Request request) throws IOException {
-        FileInputStream fis = new FileInputStream(".\\todo-app-plugin\\src\\main\\resources\\app.html");
-        BufferedReader bf = new BufferedReader(new InputStreamReader(fis, "UTF-8"));
         String template = "";
+        try(FileInputStream fis = new FileInputStream(".\\todo-app-plugin\\src\\main\\resources\\app.html");
+        BufferedReader bf = new BufferedReader(new InputStreamReader(fis, "UTF-8"))){
         String rida = bf.readLine();
         while (rida != null) {
             template += rida;
             rida = bf.readLine();
         }
+    }
         Map<String, String> responseHeaders = new HashMap<>();
         byte[] body;
         String existingItems = "";
@@ -50,11 +47,6 @@ public class ToDoApp implements RequestHandler{
     @Mapping(URI="/todoapp/delete", method="POST")
     public Response deleteTask(Request request) throws UnsupportedEncodingException {
         Map<String, String> responseHeaders = new HashMap<>();
-        Map<String, String> dataMap = request.bodyToForm();
-        if (dataMap == null) {
-            return new Response(StatusCode.BAD_REQUEST, Collections.emptyMap(), null);
-        }
-        String task = dataMap.get("user_message");
         responseHeaders.put("Location", "/todoapp/form");
         return new Response(StatusCode.FOUND, responseHeaders, null);
     }
